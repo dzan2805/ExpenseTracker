d8b3484968ea1364a4ae78710542e2e9438b9dec
Le_Tieu_Khuyen <23200099@student.hcmus.edu.vn>
2026-05-22 21:46:48 +0700

    Tieu Khuyen Update Phan 2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,69 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ExpenseTracker
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            LoadFakeData();
+        }
+
+        private void LoadFakeData()
+        {
+            var transactions = new List<dynamic>
+            {
+                new
+                {
+                    Title = "Coffee",
+                    Amount = "50,000 VND",
+                    Type = "Expense",
+                    Category = "Food",
+                    Date = DateTime.Now.ToString("dd/MM/yyyy")
+                },
+
+                new
+                {
+                    Title = "Salary",
+                    Amount = "15,000,000 VND",
+                    Type = "Income",
+                    Category = "Bills",
+                    Date = DateTime.Now.ToString("dd/MM/yyyy")
+                },
+
+                new
+                {
+                    Title = "Movie Ticket",
+                    Amount = "120,000 VND",
+                    Type = "Expense",
+                    Category = "Entertainment",
+                    Date = DateTime.Now.ToString("dd/MM/yyyy")
+                },
+
+                new
+                {
+                    Title = "Bus Ticket",
+                    Amount = "10,000 VND",
+                    Type = "Expense",
+                    Category = "Transport",
+                    Date = DateTime.Now.ToString("dd/MM/yyyy")
+                },
+
+                new
+                {
+                    Title = "Freelance Work",
+                    Amount = "3,000,000 VND",
+                    Type = "Income",
+                    Category = "Shopping",
+                    Date = DateTime.Now.ToString("dd/MM/yyyy")
+                }
+            };
+
+            transactionGrid.ItemsSource = transactions;
         }
     }
 }
