98fdc7a0cf40546476d6d0771968b9593739a75c
Le_Tieu_Khuyen <23200099@student.hcmus.edu.vn>
2026-05-24 19:45:29 +0700

    Khuyen update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,60 +10,6 @@ namespace ExpenseTracker
         {
             InitializeComponent();
 
-            LoadFakeData();
-        }
-
-        private void LoadFakeData()
-        {
-            var transactions = new List<dynamic>
-            {
-                new
-                {
-                    Title = "Coffee",
-                    Amount = "45,000 VND",
-                    Type = "Expense",
-                    Category = "Food",
-                    Date = DateTime.Now.ToString("dd/MM/yyyy")
-                },
-
-                new
-                {
-                    Title = "Salary",
-                    Amount = "15,000,000 VND",
-                    Type = "Income",
-                    Category = "Bills",
-                    Date = DateTime.Now.ToString("dd/MM/yyyy")
-                },
-
-                new
-                {
-                    Title = "Movie Ticket",
-                    Amount = "120,000 VND",
-                    Type = "Expense",
-                    Category = "Entertainment",
-                    Date = DateTime.Now.ToString("dd/MM/yyyy")
-                },
-
-                new
-                {
-                    Title = "Bus Ticket",
-                    Amount = "10,000 VND",
-                    Type = "Expense",
-                    Category = "Transport",
-                    Date = DateTime.Now.ToString("dd/MM/yyyy")
-                },
-
-                new
-                {
-                    Title = "Freelance Work",
-                    Amount = "3,000,000 VND",
-                    Type = "Income",
-                    Category = "Shopping",
-                    Date = DateTime.Now.ToString("dd/MM/yyyy")
-                }
-            };
-
-            transactionGrid.ItemsSource = transactions;
         }
     }
 }
